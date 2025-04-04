2a91dbe434c2e12725b0ec06deef9178b1787e56
Daniil Sivakov <daniil.sivakov.work@gmail.com>
2025-04-04 07:42:03 +0300

    feat: implement home screen

--- lib/presentation/screens/home_screen/home_screen.dart
+++ lib/presentation/screens/home_screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:rick_and_morty_app/presentation/localization/localization_plug.dart';
+import 'package:rick_and_morty_app/presentation/navigation/app_router.gr.dart';
 
 @RoutePage()
 class HomeScreen extends StatelessWidget {
@@ -7,6 +9,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return AutoTabsScaffold(
+      appBarBuilder: (_, _) => AppBar(title: Text(LocalizationPlug.appTitle)),
+      routes: [CharacterListRoute(), SettingsRoute()],
+      bottomNavigationBuilder: (_, router) {
+        return BottomNavigationBar(
+          currentIndex: router.activeIndex,
+          onTap: router.setActiveIndex,
+          items: [
+            BottomNavigationBarItem(
+              label: LocalizationPlug.charactersTabLabel,
+              icon: Icon(Icons.list),
+            ),
+            BottomNavigationBarItem(
+              label: LocalizationPlug.settingsTabLabel,
+              icon: Icon(Icons.settings),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
